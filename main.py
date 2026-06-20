5e2d34e708449c04e77262b6db457353882be32c
Raghav893 <raghavarora2309@gmail.com>
2026-06-20 13:28:41 +0530

    feat: initialize FastAPI app with OpenAI client and add .env to gitignore

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os 
+from openai import OpenAI
+
+
+app = FastAPI()
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+@app.get("/")
+def getKey():
+    return  os.getenv("OPENAI_API_KEY")
